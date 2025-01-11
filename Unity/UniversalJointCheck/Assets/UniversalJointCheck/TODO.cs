66af1d9e765527973e344bf8d02cd268cf170579
T-Kuhn <tkuhn.mpu@gmail.com>
2025-01-11 16:24:07 +0900

    TODO

--- Unity/UniversalJointCheck/Assets/UniversalJointCheck/TODO.cs
+++ Unity/UniversalJointCheck/Assets/UniversalJointCheck/TODO.cs
@@ -3,7 +3,7 @@
 // - [X] make our current arm work no matter how it is rotated (calculations need to be in local space)
 // - [X] make our current arm work no matter where it is placed
 // - [X] would be nice if we had gizmo lines for all joints maybe?
-// - [ ] also make upper joint move (keep this one parallel to floor)
+// - [X] also make upper joint move (keep this one parallel to floor)
 // - [ ] add some sort of path/animation for target position maybe
 // - [ ] add more arms
 // - [ ] iterate on arm design based on what we'll see in the simulated machine
