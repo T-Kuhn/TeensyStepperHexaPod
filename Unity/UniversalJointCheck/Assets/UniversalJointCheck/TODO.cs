6fb20ba19d85d1f984e26c4f71cd9ccb6bb6284d
T-Kuhn <tkuhn.mpu@gmail.com>
2025-01-11 21:53:00 +0900

    add hexaplate model

--- Unity/UniversalJointCheck/Assets/UniversalJointCheck/TODO.cs
+++ Unity/UniversalJointCheck/Assets/UniversalJointCheck/TODO.cs
@@ -4,11 +4,16 @@
 // - [X] make our current arm work no matter where it is placed
 // - [X] would be nice if we had gizmo lines for all joints maybe?
 // - [X] also make upper joint move (keep this one parallel to floor)
-// - [ ] add some sort of path/animation for target position maybe
-// - [ ] add more arms
+// - [ ] automatically instanciate arms at sensible positions
+//     - [ ] instantiate end effector platform on play
+//     - [ ] instantiate 6 arms on play (setup target positions inside end effector platform)
+//     - [ ] parameters for easy experimentation:
+//           - distance from center for motors pairs at midpoint
+//           - distance apart of motor pair
+//           - distance from center for target pairs below endeffector at midpoint
+//           - distance apart of target pair
+// - [ ] add some sort of animation for end effector platform
 // - [ ] iterate on arm design based on what we'll see in the simulated machine
-// - [ ] automatically instanciate arms at sensible positions (use parameters)
-
 // - [ ] Need a name for the thing. Some ideas:
 //   - u-joint. The UJ-Table
 //   - hexapod. The Hexa Bot.
